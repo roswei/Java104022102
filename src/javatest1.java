3e0926bc095d3a27cf5f2729f8d60457565ba4f3
roswie <1040222102@live.asia.edu.tw>
2017-09-25 15:19:27 +0800

    frist test

--- src/javatest1.java
+++ src/javatest1.java
@@ -1,6 +1,6 @@
 public class javatest1 {
 
         public static void main(String[] args) {
-            System.out.println("第一個Java程式");
+            System.out.println("第2個Java程式");
         }
 }
